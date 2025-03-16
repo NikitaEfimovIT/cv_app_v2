0251249ae2b7a26ce4ade1e8a9a9687154d8d5ec
NikitaEfimovIT <nikita.efimov.it@gmail.com>
2025-03-16 14:04:58 +0300

    added requered libraries and started development

---
+++ src/utils/colors.ts
@@ -0,0 +1,7 @@
+export const appColors = {
+  text: "#fff",
+  background: "#000",
+  strokeInline: "linear-gradient(90deg, #000000 0%, #FFFFFF 100%)",
+  headerBackground: "linear-gradient(13deg, rgba(0, 0, 0, 0.2) 0%, rgba(255, 255, 255, 0.2) 100%)",
+  cardBackground: "linear-gradient(90deg, rgba(0, 0, 0, 0.4) 0%, rgba(34, 34, 34, 0.2) 50%, rgba(61, 61, 61, 0.4) 100%);"
+}
